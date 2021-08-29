c2ae823c8f0241f4e4c8e40849d11171d27aee53
sarthakdharia <71364213+sarthakdharia@users.noreply.github.com>
2021-08-29 16:20:59 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/movify/LoginActivity2.java
@@ -0,0 +1,21 @@
+package com.example.movify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+
+    public void btnBack(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/movify/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.movify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+            }
+        });
+    }
+    public void openLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity2.class);
+        startActivity(intent);
+    }
+}
